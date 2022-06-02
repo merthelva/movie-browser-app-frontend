8446800b8db10eb6a553849e2e5c6184348eb934
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-02 21:07:33 +0300

    move main content just below fixed header

--- layouts/MainLayout/styles.ts
+++ layouts/MainLayout/styles.ts
@@ -2,4 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div``;
 
-export const Main = styled.main``;
+export const Main = styled.main`
+  position: relative;
+  top: 60px;
+`;
