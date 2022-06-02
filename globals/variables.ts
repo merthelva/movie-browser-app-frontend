b2c775940877a38a28f1f9a9f1c282306d9aec45
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-02 20:49:21 +0300

    add font weight CSS variables

--- globals/variables.ts
+++ globals/variables.ts
@@ -1,5 +1,12 @@
 import { css } from "styled-components";
 
 export default css`
-  //:root {}
+  :root {
+    /* Font Weights */
+    --fw-thin: 300;
+    --fw-normal: 400;
+    --fw-semi: 500;
+    --fw-bold: 600;
+    --fw-bolder: 700;
+  }
 `;
