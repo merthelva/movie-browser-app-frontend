82d71f0cb8f697117096e28eb0fd7f693a4355c6
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-03 10:51:25 +0300

    add backdrop portal

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -54,6 +54,7 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
+          <div id="backdrop" />
           <Main />
           <NextScript />
         </body>
