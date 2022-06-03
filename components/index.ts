5da9284a1d810df1eb618688c668670135b79ac8
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-03 10:52:45 +0300

    export default for Backdrop and Drawer

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
+export { default as Backdrop } from "./Backdrop";
+export { default as Drawer } from "./Drawer";
 export { default as DrawerButton } from "./DrawerButton";
 export { default as Header } from "./Header";
 export { default as Icon } from "./Icon";
