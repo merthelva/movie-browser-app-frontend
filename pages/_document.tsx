4e04d68c203a4586de7f468353f2ed054dd691ca
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-04 12:47:21 +0300

    add Acme font for logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -49,7 +49,7 @@ export default class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300,400,500,600,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Acme&family=Roboto:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
