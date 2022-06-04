c0f62c3d019dea6ca95912e788fb340587e94efa
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-04 11:41:00 +0300

    add z-index and background-color styles to Header

--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
+import { ZIndices } from "lib/constants";
+
 export const Header = styled.header`
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
+  z-index: ${ZIndices.HEADER};
   height: 60px;
   padding: 0 var(--spacing-8x);
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.dark};
+  background-color: ${({ theme }) => theme.colors.light};
 `;
