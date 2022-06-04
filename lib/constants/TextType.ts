85d2ad4ffe2fd0651eecbe889322b5d65c36ccb8
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-04 11:38:30 +0300

    add TextType component

---
+++ lib/constants/TextType.ts
@@ -0,0 +1,6 @@
+enum TextType {
+  TEXT = "Text",
+  PARAGRAPH = "Paragraph",
+}
+
+export default TextType;
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,4 +1,5 @@
 export { default as Colors } from "./Colors";
 export { default as IconSize } from "./IconSize";
 export { default as SvgIcon } from "./SvgIcon";
+export { default as TextType } from "./TextType";
 export { default as ZIndices } from "./ZIndices";
