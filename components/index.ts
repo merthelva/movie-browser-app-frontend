02cf36eac290dda96892771d65d11f0fb628a312
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-04 11:40:15 +0300

    export components

--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,6 @@ export { default as Drawer } from "./Drawer";
 export { default as DrawerButton } from "./DrawerButton";
 export { default as Header } from "./Header";
 export { default as Icon } from "./Icon";
+export { default as MovieCard } from "./MovieCard";
+export { default as MovieRate } from "./MovieRate";
+export { default as Text } from "./Text";
