8a9dab4b7facb5f71190242d6a6889624e65a1f1
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-04 11:41:12 +0300

    add body background-color

--- globals/GlobalStyles.ts
+++ globals/GlobalStyles.ts
@@ -14,6 +14,7 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: 'Roboto', sans-serif;
     font-weight: var(--fw-normal);
+    background-color: #393E46;
   }
 
   p, h1, h2, h3, h4, h5, h6 {
