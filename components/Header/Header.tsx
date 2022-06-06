238547de4bad6d61a943aa49188cdf6efa893a8e
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-06 18:23:40 +0300

    add components to Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+
 import * as S from "./styles";
 
+import { Colors, SvgIcon } from "lib/constants";
+
+import Icon from "../Icon";
+import Text from "../Text";
+import DrawerButton from "../DrawerButton";
+
 const Header: React.FC = () => {
-  return <S.Header />;
+  return (
+    <S.Header>
+      <Link href="">
+        <S.LogoWrapper>
+          <Icon name={SvgIcon.VIDEO} color={Colors.PRIMARY} />
+          <Text>MovieBrowser</Text>
+        </S.LogoWrapper>
+      </Link>
+      <DrawerButton />
+    </S.Header>
+  );
 };
 
 export default Header;
