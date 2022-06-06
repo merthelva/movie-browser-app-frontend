aeaddf2318c16a0ff49f041f1ea454ee9e4e2431
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-06 18:22:59 +0300

    update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import App, { AppInitialProps } from "next/app";
-//import { Provider } from "react-redux";
 
-//import { store } from "../store";
+import { wrapper } from "../store";
 import { MainLayout } from "../layouts";
 import { AppPropsWithLayout } from "./app.types";
 import { GlobalStyles, ThemeProvider, Variables } from "../globals";
-import { wrapper } from "../store";
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout & AppInitialProps) {
   // Use the layout defined at the page level, if available
@@ -14,18 +12,16 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout & AppInitialProps) {
 
   return (
     <>
-      {/* <Provider store={store}> */}
       <Variables />
       <GlobalStyles />
       <ThemeProvider>{getLayout(<Component {...pageProps} />)}</ThemeProvider>
-      {/* </Provider> */}
     </>
   );
 }
 
 MyApp.getInitialProps = wrapper.getInitialAppProps(
   (store) => async (context) => {
-    //store.dispatch({ type: "PRINT_TO_CONSOLE", payload: {} });
+    //store.dispatch(GenresActions.fetchGenresRequest());
 
     return {
       pageProps: {
