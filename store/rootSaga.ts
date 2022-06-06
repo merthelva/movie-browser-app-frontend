72ae5d21c5be71bfe9ac13aa869a184a2f12ab75
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-06 11:01:39 +0300

    run moviesRootSaga

--- store/rootSaga.ts
+++ store/rootSaga.ts
@@ -1,9 +1,7 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all } from "redux-saga/effects";
 
-function* helloSaga() {
-  console.log("Hello Sagas!");
-}
+import { moviesRootSaga } from "./slices";
 
 export default function* rootSaga() {
-  yield all([takeLatest("PRINT_TO_CONSOLE", helloSaga)]);
+  yield all([moviesRootSaga()]);
 }
