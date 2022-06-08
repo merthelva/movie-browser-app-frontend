f717082656269452e2cb5735a64917b1ab3f67d4
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-08 15:41:18 +0300

    replace static rem value with CSS variable

--- pages/styles.ts
+++ pages/styles.ts
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
-  gap: 1.5rem;
+  gap: var(--spacing-12x);
   margin-bottom: var(--spacing-12x);
 `;
