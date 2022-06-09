e5143c56fb48339dec59dd708ef3d5941e340221
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-09 09:46:36 +0300

    change media breakpoint for DrawerButton display

--- components/DrawerButton/styles.ts
+++ components/DrawerButton/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { Breakpoints } from "lib/constants";
 
 export const Wrapper = styled.button`
-  @media (min-width: ${Breakpoints.LAPTOP.MIN}px) {
+  @media (min-width: ${Breakpoints.TABLET.MIN}px) {
     display: none;
   }
 `;
