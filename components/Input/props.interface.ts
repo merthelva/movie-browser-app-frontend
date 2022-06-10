f73e0fefdad7b89d626e2c00d4b93d967415c1fa
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-10 11:02:40 +0300

    add "onChange" and "value" props

--- components/Input/props.interface.ts
+++ components/Input/props.interface.ts
@@ -6,9 +6,11 @@ export interface IProps {
   id: string;
   isAutoFocused?: boolean;
   label: string;
+  onChange?: (e: React.FormEvent) => void;
   placeholder?: string;
   size?: InputSize;
   type?: InputType;
+  value?: string;
 }
 
 export interface IWrapperProps {
