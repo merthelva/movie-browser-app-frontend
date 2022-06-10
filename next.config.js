e23a580aca664f27e8a57bd13f94cf501ffbfddc
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-10 15:24:42 +0300

    add new environment variables

--- next.config.js
+++ next.config.js
@@ -5,8 +5,9 @@ const nextConfig = {
     domains: ["image.tmdb.org"],
   },
   env: {
-    BASE_URL: process.env.BASE_URL,
-    API_KEY: process.env.API_KEY,
+    TMDB_BASE_URL: process.env.TMDB_BASE_URL,
+    TMDB_API_KEY: process.env.TMDB_API_KEY,
+    MONGODB_BASE_URL: process.env.MONGODB_BASE_URL,
   },
 };
 
