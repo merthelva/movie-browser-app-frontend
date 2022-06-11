d762f8068200a2ced2425fc47316a2e638c46c0f
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-11 14:52:57 +0300

    define IFieldSetterMap interface

--- pages/auth/props.interface.ts
+++ pages/auth/props.interface.ts
@@ -1,3 +1,9 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface IFormWrapperProps {
   isLoading?: boolean;
+}
+
+export interface IFieldSetterMap {
+  [key: string]: Dispatch<SetStateAction<string>>;
 }
