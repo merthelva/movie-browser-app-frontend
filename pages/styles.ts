2b81783665c508b3515bce5ef63f39b7d77c82c3
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-13 10:40:05 +0300

    update display of MovieCard component layout

--- pages/styles.ts
+++ pages/styles.ts
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 
+import { Breakpoints } from "lib/constants";
+
 export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
   gap: var(--spacing-12x);
   margin-bottom: var(--spacing-12x);
+
+  @media (min-width: 414px) and (max-width: ${Breakpoints.MOBILE.MAX}px) {
+    justify-content: center;
+    grid-template-columns: 320px;
+  }
 `;
