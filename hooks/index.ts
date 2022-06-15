32a5d4406f5bbadce364249a8f3c5eff1624f411
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-15 22:32:35 +0300

    add useIsMounted hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,4 @@
+export { default as useToggle } from "./useToggle";
+export { default as useIsMounted } from "./useIsMounted";
 export { useAppDispatch, useAppSelector } from "./useStore";
 export { default as useImageGallery } from "./useImageGallery";
-export { default as useToggle } from "./useToggle";
