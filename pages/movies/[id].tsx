9ae83c968c5bd93edba6e9b270c3bf3e9e8d4f77
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-17 11:53:13 +0300

    resolve TODO items in /movies/:id page

--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -23,7 +23,6 @@ import {
   Text,
 } from "components";
 
-// TODO: Collect and group rendered parts and business logic into sub groups for better readability
 const MovieDetailsPage: NextPage<IPageProps> = ({
   id,
   coverImageSrc,
@@ -55,17 +54,18 @@ const MovieDetailsPage: NextPage<IPageProps> = ({
   }, [isToggled, movieCast]);
 
   const handleAddMovieToWatchlist = () => {
-    // TODO: Consider replacing the logic of dispatching the action directly with checking first that if the movie with "id" is already in watchlist
-    const movie = {
-      id,
-      budget,
-      releaseDate,
-      duration,
-      title,
-      rate,
-    };
+    if (!isMovieInWatchlist) {
+      const movie = {
+        id,
+        budget,
+        releaseDate,
+        duration,
+        title,
+        rate,
+      };
 
-    dispatch(UserActions.addMovieToWatchlistRequest(movie, userId));
+      dispatch(UserActions.addMovieToWatchlistRequest(movie, userId));
+    }
   };
 
   const handleRemoveMovieFromWatchlist = () => {
@@ -79,6 +79,90 @@ const MovieDetailsPage: NextPage<IPageProps> = ({
 
   if (!isMounted) return null;
 
+  const renderMovieDetails = (
+    <S.Details>
+      <S.Body>
+        <S.Content isLoading={watchlistStatus === Status.LOADING}>
+          <S.ContentHeader>
+            <Text>{title}</Text>
+            <MovieRate rate={rate} />
+          </S.ContentHeader>
+          <Button
+            kind={ButtonType.PRIMARY}
+            size={ButtonSize.MEDIUM}
+            onClick={handleToggleMovieInWatchlist}
+          >
+            {watchlistStatus === Status.LOADING ? (
+              <Spinner size={14} thickness={3} />
+            ) : (
+              <Icon
+                name={
+                  isMovieInWatchlist
+                    ? SvgIcon.BOOKMARK_FILLED
+                    : SvgIcon.BOOKMARK_OUTLINED
+                }
+                color={Colors.LIGHT}
+                size={16}
+              />
+            )}
+            <Text>{`${
+              isMovieInWatchlist ? "Remove from" : "Add to"
+            } Watchlist`}</Text>
+          </Button>
+          <Text>{overview}</Text>
+        </S.Content>
+        <S.PosterWrapper>
+          <S.Poster
+            alt={title}
+            width={300}
+            height={450}
+            objectFit="cover"
+            src={`https://image.tmdb.org/t/p/w300/${coverImageSrc}`}
+          />
+        </S.PosterWrapper>
+        <S.Footer>
+          <S.FooterInfo>
+            <Icon name={SvgIcon.CLOCK} color={Colors.LIGHT} size={20} />
+            <Text>{releaseDate}</Text>
+          </S.FooterInfo>
+          <S.FooterInfo>
+            <Icon name={SvgIcon.HISTORY} color={Colors.LIGHT} size={20} />
+            <Text>{duration} min.</Text>
+          </S.FooterInfo>
+          <S.FooterInfo>
+            <Icon name={SvgIcon.MONEY} color={Colors.LIGHT} size={20} />
+            <Text>$ {convertLongNumberToReadableFormat(budget, {})}</Text>
+          </S.FooterInfo>
+        </S.Footer>
+      </S.Body>
+    </S.Details>
+  );
+
+  const renderActors = (
+    <S.Actors>
+      <S.ActorsHeader>
+        <Text>ACTORS</Text>
+        <S.SwitchWrapper>
+          <Switch isToggled={isToggled} onToggle={handleToggle} />
+          <Text>Show all</Text>
+        </S.SwitchWrapper>
+      </S.ActorsHeader>
+      <S.CastWrapper>
+        {movieCast.slice(0, numberOfCastsToBeRendered).map((person: any) => {
+          if (!person.avatar) return null;
+          return (
+            <CharacterDetail
+              key={person.id}
+              avatarSrc={person.avatar}
+              name={person.name}
+              character={person.character}
+            />
+          );
+        })}
+      </S.CastWrapper>
+    </S.Actors>
+  );
+
   return (
     <>
       <Notification
@@ -86,86 +170,8 @@ const MovieDetailsPage: NextPage<IPageProps> = ({
         notificationText={errors?.userId.message}
       />
       <S.Wrapper>
-        <S.Details>
-          <S.Body>
-            <S.Content isLoading={watchlistStatus === Status.LOADING}>
-              <S.ContentHeader>
-                <Text>{title}</Text>
-                <MovieRate rate={rate} />
-              </S.ContentHeader>
-              <Button
-                kind={ButtonType.PRIMARY}
-                size={ButtonSize.MEDIUM}
-                onClick={handleToggleMovieInWatchlist}
-              >
-                {watchlistStatus === Status.LOADING ? (
-                  <Spinner size={14} thickness={3} />
-                ) : (
-                  <Icon
-                    name={
-                      isMovieInWatchlist
-                        ? SvgIcon.BOOKMARK_FILLED
-                        : SvgIcon.BOOKMARK_OUTLINED
-                    }
-                    color={Colors.LIGHT}
-                    size={16}
-                  />
-                )}
-                <Text>{`${
-                  isMovieInWatchlist ? "Remove from" : "Add to"
-                } Watchlist`}</Text>
-              </Button>
-              <Text>{overview}</Text>
-            </S.Content>
-            <S.PosterWrapper>
-              <S.Poster
-                alt={title}
-                width={300}
-                height={450}
-                objectFit="cover"
-                src={`https://image.tmdb.org/t/p/w300/${coverImageSrc}`}
-              />
-            </S.PosterWrapper>
-            <S.Footer>
-              <S.FooterInfo>
-                <Icon name={SvgIcon.CLOCK} color={Colors.LIGHT} size={20} />
-                <Text>{releaseDate}</Text>
-              </S.FooterInfo>
-              <S.FooterInfo>
-                <Icon name={SvgIcon.HISTORY} color={Colors.LIGHT} size={20} />
-                <Text>{duration} min.</Text>
-              </S.FooterInfo>
-              <S.FooterInfo>
-                <Icon name={SvgIcon.MONEY} color={Colors.LIGHT} size={20} />
-                <Text>$ {convertLongNumberToReadableFormat(budget, {})}</Text>
-              </S.FooterInfo>
-            </S.Footer>
-          </S.Body>
-        </S.Details>
-        <S.Actors>
-          <S.ActorsHeader>
-            <Text>ACTORS</Text>
-            <S.SwitchWrapper>
-              <Switch isToggled={isToggled} onToggle={handleToggle} />
-              <Text>Show all</Text>
-            </S.SwitchWrapper>
-          </S.ActorsHeader>
-          <S.CastWrapper>
-            {movieCast
-              .slice(0, numberOfCastsToBeRendered)
-              .map((person: any) => {
-                if (!person.avatar) return null;
-                return (
-                  <CharacterDetail
-                    key={person.id}
-                    avatarSrc={person.avatar}
-                    name={person.name}
-                    character={person.character}
-                  />
-                );
-              })}
-          </S.CastWrapper>
-        </S.Actors>
+        {renderMovieDetails}
+        {renderActors}
       </S.Wrapper>
     </>
   );
