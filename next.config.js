e82c7ac740c4bdd7c36eef3ae27ddf5744ea0b78
merthelva <mert.helvaci92@gmail.com>
2022-06-19 22:42:00 +0300

    configure and add ".page" extension to all page components

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     domains: ["image.tmdb.org"],
   },
+  pageExtensions: ["page.tsx"],
   env: {
     TMDB_BASE_URL: process.env.TMDB_BASE_URL,
     TMDB_API_KEY: process.env.TMDB_API_KEY,
