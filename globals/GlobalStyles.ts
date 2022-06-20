3a916d17e026c1675b2cef2c3b0d7949bf9c2ef8
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2022-06-20 19:01:50 +0300

    update GlobalStyles

--- globals/GlobalStyles.ts
+++ globals/GlobalStyles.ts
@@ -5,18 +5,19 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     padding: 0;
     margin: 0;
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
   }
-
+  
   html {
     scroll-behavior: smooth;
   }
-
+  
   body {
-    font-family: 'Roboto', sans-serif;
-    font-weight: var(--fw-normal);
     background-color: #393E46;
+    font-weight: var(--fw-normal);
   }
-
+  
   p, h1, h2, h3, h4, h5, h6 {
     margin-bottom: var(--spacing-4x);
   }
